3233427d1748980ef12f177b85a8af4d8ee54260
João Paulo <joao.elesbao@g.globo.com>
2023-01-19 20:14:59 -0300

    feat(breakpoint): adicionado breakpoints

---
+++ src/common/breakpoints.ts
@@ -0,0 +1,18 @@
+export const BREAKPOINTS = {
+  xxs: "350px",
+  xss: " 376px",
+  xsspecific: "420px",
+  xs: "576px",
+  smspecific: "600px",
+  smspecifictable: "650px",
+  smedium: "750px",
+  sm: "800px",
+  md: "992px",
+  big: "1060px",
+  ipadPro: "1160px",
+  largeScreen: "1200px",
+  specific: "1325px",
+  lg: "1200px",
+  lgg: "1300px",
+  xg: "1370px",
+};
