9a8f6e7b8abcc80a0d5fa1945a7162dffb109978
João Paulo <joao.elesbao@g.globo.com>
2023-01-20 20:48:32 -0300

    feat:(header): ajuste props

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -4,10 +4,10 @@ import { CardButton } from "./style";
 interface ButtonProps {
   children: ReactNode;
   onClick: () => void;
-  padding?:string
+  padding?: string;
 }
 
-const Button = ({ children, onClick,padding }: ButtonProps) => {
+const Button = ({ children, onClick, padding }: ButtonProps) => {
   return (
     <CardButton padding={padding}>
       <button onClick={onClick}>{children}</button>
