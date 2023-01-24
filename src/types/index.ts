1c304c665ea06727d3f86d8537f7a2a47c124e2b
João Paulo <joao.elesbao@g.globo.com>
2023-01-23 21:28:33 -0300

    feat(types):adicionando types de produto e usuário

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,8 @@
-import { ReactNode } from "react";
+import { ChangeEvent, ReactNode } from "react";
 
 export interface InputProps {
   type: string;
-  onChange: any;
+  onChange: ChangeEvent<HTMLInputElement> | any;
   value: string;
   placeholder: string;
   name: string;
@@ -12,4 +12,42 @@ export interface ButtonProps {
   children: ReactNode;
   onClick: () => void;
   padding?: string;
+}
+
+export interface UserProps {
+  id: number;
+  office: "admin" | "user";
+  fullName: string;
+  email: string;
+  state: string;
+  city: string;
+  district: string;
+  address: string;
+  numberAddress?: number;
+  phone: number;
+  saleScore?: number;
+  shopingScore?: number;
+  totalScore: number;
+}
+
+export interface ProductProps {
+  id: number;
+  category: "caprino" | "ovino";
+  breed: string;
+  name: string;
+  currentImage: string;
+  images?: [
+    {
+      id: number;
+      link: string;
+    }
+  ];
+  video?: {
+    id: number;
+    link: string;
+  };
+  description: string;
+  document: string;
+  age: string;
+  price: number;
 }
