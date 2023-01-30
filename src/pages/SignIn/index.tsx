44700415e8896726b6efacaad20df43964d9545e
Lucas Henrique <lukas.paes18@gmail.com>
2023-01-30 12:59:37 -0300

    feature: header style

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,58 +1,58 @@
-import { Button } from "@/components";
-import { Input } from "@/components/atoms/Input/style";
+import {Button} from '@/components';
+import {Input} from '@/components/atoms/Input/style';
 
-import { ChangeEvent, FormEvent, useState } from "react";
+import {ChangeEvent, FormEvent, useState} from 'react';
 
 const SignIn = () => {
   const [form, setForm] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setForm((prevForm) => ({ ...prevForm, [name]: value }));
+    const {name, value} = event.target;
+    setForm((prevForm) => ({...prevForm, [name]: value}));
   };
 
   const submitForm = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setForm({ email: "", password: "" });
-    console.log("Formulário enviado", form);
+    setForm({email: '', password: ''});
+    console.log('Formulário enviado', form);
   };
 
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "500px",
-        margin: "auto",
-        height: "70vh",
+        display: 'flex',
+        flexDirection: 'column',
+        width: '500px',
+        margin: 'auto',
+        height: '70vh',
       }}
     >
       <form
         onSubmit={submitForm}
         style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "500px",
-          margin: "auto",
-          gap: "5px",
+          display: 'flex',
+          flexDirection: 'column',
+          width: '500px',
+          margin: 'auto',
+          gap: '5px',
         }}
       >
         <h2>Entrar</h2>
         <Input
-          name="email"
-          type="email"
-          placeholder="E-mail"
+          name='email'
+          type='email'
+          placeholder='E-mail'
           required
           value={form.email}
           onChange={handleChange}
         />
         <Input
-          name="password"
-          type="password"
-          placeholder="Senha"
+          name='password'
+          type='password'
+          placeholder='Senha'
           value={form.password}
           onChange={handleChange}
         />
