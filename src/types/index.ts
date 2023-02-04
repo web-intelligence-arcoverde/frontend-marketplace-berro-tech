79d8c6054c8e7670c7a687ed6be93947d3aef3b2
Gab096 <Gabrielnogueira0000@hotmail.com>
2023-02-04 12:19:59 -0300

    fix(SellerCard):componentizando CardContact

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,4 +2,5 @@ export type { ButtonProps } from "./IButtonProps";
 export type { UserProps } from "./IUserProps";
 export type { ProductProps } from "./IProductProps";
 export type { CardProductProps } from "./ICardProductProps";
+export type { CardContactProps } from "./ICardContactProps";
 export type { CardNameLocationProps } from "./ICardNameLocationProps";
