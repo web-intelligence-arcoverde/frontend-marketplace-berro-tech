6a72975295109576e5c2e149c5a1daf6db2e07ac
Lucas Henrique <lukas.paes18@gmail.com>
2023-02-06 17:37:03 -0300

    fix: ajuste para rodar a build

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,6 +1,5 @@
-export { default as Logo } from "./Logo";
-export { default as Button } from "./Button";
-export { default as Search } from "./Search";
-export { default as CardNameLocation } from "./CardNameLocation";
-export { default as CardContact } from "./CardContact";
-export { default as Avaliation } from "./Avaliation";
+export {default as Logo} from './Logo';
+export {default as Button} from './Button';
+export {default as Search} from './Search';
+export {default as CardContact} from './CardContact';
+export {default as Avaliation} from './Avaliation';
---
+++ src/store/reducer/counter/initial.ts
@@ -0,0 +1,7 @@
+type CounterState = {
+  value: number;
+};
+
+export const initialState: CounterState = {
+  value: 0,
+};
