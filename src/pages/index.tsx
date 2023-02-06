be2c2ed5b844b2d8c9c48b055959cf2d5145c5ac
Lucas Henrique <lukas.paes18@gmail.com>
2023-02-06 17:38:45 -0300

    fix: ajuste para rodar a build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,12 @@ import {Header, Presentation, PresentationInfo} from '@/components';
 import {useAppSelector, useAppDispatch} from '@/hooks/useSelectorHook';
 import {Main} from '@/style';
 
-import {increment} from '@/store/reducer/counter/actions';
-import {getKanyeQuote} from '@/store/reducer/kenye/reducer';
-
 const Home = () => {
   const state = useAppSelector((state) => state);
-  const dispatch = useAppDispatch();
-
-  const {data, pending, error} = useAppSelector((state) => state.kenye);
-  console.log(data);
+  console.log(state);
 
   return (
     <Main>
-      <button onClick={() => dispatch(getKanyeQuote())} disabled={pending}>
-        Generate Kanye Quote
-      </button>
-      <button onClick={() => dispatch(increment())}>example</button>
       <Header />
       <Presentation />
       <PresentationInfo />
