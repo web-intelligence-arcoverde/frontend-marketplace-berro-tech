770d1c0076316c445891c420d98a022a6d4e29a7
João Paulo <joao.elesbao@g.globo.com>
2023-02-06 15:11:39 -0300

    fix(logo): mudado a logo conforme o cliente solicitou

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { IMAGES } from "../../../assets/images";
 
 const Logo = () => {
-  return <Image width={40} src={IMAGES.Logo} alt="logo berro tech" />;
+  return <Image width={180} src={IMAGES.Logo} alt="logo berro tech" />;
 };
 
 export default Logo;
