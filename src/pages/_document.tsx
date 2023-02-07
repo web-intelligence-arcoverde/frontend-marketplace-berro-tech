957e89f5106d59b8527353fb747c8a1e6c87bfa3
João Paulo <joao.elesbao@g.globo.com>
2023-02-07 11:13:56 -0300

    fix(css): ajuste para o css carregar antes de mostrar para o usuário

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,32 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <title>Berro Tech</title>
-        <link rel="icon" href="./favicon.png" />
-        <meta name="viewport" content="viewport-fit=cover" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }
