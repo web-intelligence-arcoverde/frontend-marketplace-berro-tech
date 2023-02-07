ecdd24637028149202e9cfed712e4c9a9c67d8a4
João Paulo <joao.elesbao@g.globo.com>
2023-02-07 19:48:23 -0300

    fix:(presentation-info): ajuste icone e importação de mock

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,23 +1,44 @@
-import {ICONS} from '@/assets';
+import { ICONS } from "@/assets";
 
 export const NavigationHeader = [
-  {name: 'favorito', icon: ICONS.Star},
-  {name: 'mensagens', icon: ICONS.Message},
-  {name: 'alerta', icon: ICONS.Alert},
+  { name: "favorito", icon: ICONS.Star },
+  { name: "mensagens", icon: ICONS.Message },
+  { name: "alerta", icon: ICONS.Alert },
 ];
 
 export const MocksCard: any[] = [
   {
     photo:
-      'https://www.petz.com.br/blog/wp-content/uploads/2022/05/diferenca-entre-boi-e-touro-topo.jpg',
-    breed: 'senepol',
+      "https://www.petz.com.br/blog/wp-content/uploads/2022/05/diferenca-entre-boi-e-touro-topo.jpg",
+    breed: "senepol",
     quantity: 3,
-    name: 'Garrote Senepol',
-    city: 'Campina Grande',
-    state: 'Paraiba',
-    country: 'Brasil',
-    sex: 'Macho',
-    age: '3 anos',
-    rank: 'Genética',
+    name: "Garrote Senepol",
+    city: "Campina Grande",
+    state: "Paraiba",
+    country: "Brasil",
+    sex: "Macho",
+    age: "3 anos",
+    rank: "Genética",
+  },
+];
+
+export const PresentationInfoCardMock = [
+  {
+    icon: ICONS?.SearchBlue,
+    iconName: "procura",
+    title: "Encontre animais",
+    description: "Busque por caprinos e ovinos",
+  },
+  {
+    icon: ICONS?.Document,
+    iconName: "Documento",
+    title: "Consulte as informações",
+    description: "De cada produto facilmente",
+  },
+  {
+    icon: ICONS?.Phone,
+    iconName: "telefone",
+    title: "Entre em contato",
+    description: "E negocie com o vendedor",
   },
 ];
