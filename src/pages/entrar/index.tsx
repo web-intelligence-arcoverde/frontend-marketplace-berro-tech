97f12e09ada783f43f252b8d4c6c64e6e467e56e
João Paulo <joao.elesbao@g.globo.com>
2023-02-08 15:03:09 -0300

    feat(login): criado estado para escolher login com email

--- src/pages/entrar/index.tsx
+++ src/pages/entrar/index.tsx
@@ -1,10 +1,12 @@
-import { Button, LayoutInit, MiniContainer } from "@/components";
+import { Button, ButtonLink, LayoutInit, MiniContainer } from "@/components";
 import InputUser from "@/components/atoms/Input/InputUser";
 
 import { ChangeEvent, FormEvent, useState } from "react";
 import { FormLogin, LostPassword } from "./style";
 
 const SignIn = () => {
+  const [loginWithEmail, setLoginWithEmail] = useState(false);
+
   const [form, setForm] = useState({
     email: "",
     password: "",
@@ -29,25 +31,37 @@ const SignIn = () => {
         lastButton="Criar conta"
         lastButtonLink="criar-conta"
       >
-        <FormLogin onSubmit={submitForm}>
-          <InputUser
-            name="email"
-            type="email"
-            placeholder="E-mail"
-            required
-            value={form.email}
-            onChange={handleChange}
-          />
-          <InputUser
-            name="password"
-            type="password"
-            placeholder="Senha"
-            value={form.password}
-            onChange={handleChange}
-          />
-          <LostPassword href="">Esqueci minha senha</LostPassword>
-          <Button onClick={console.log}>Entrar</Button>
-        </FormLogin>
+        {loginWithEmail ? (
+          <FormLogin onSubmit={submitForm}>
+            <InputUser
+              name="email"
+              type="email"
+              placeholder="E-mail"
+              required
+              value={form.email}
+              onChange={handleChange}
+            />
+            <InputUser
+              name="password"
+              type="password"
+              placeholder="Senha"
+              value={form.password}
+              onChange={handleChange}
+            />
+            <LostPassword href="">Esqueci minha senha</LostPassword>
+            <Button onClick={console.log}>Entrar</Button>
+          </FormLogin>
+        ) : (
+          <FormLogin>
+            <Button
+              onClick={() => {
+                setLoginWithEmail(true);
+              }}
+            >
+              Entrar com Email
+            </Button>
+          </FormLogin>
+        )}
       </MiniContainer>
     </LayoutInit>
   );
