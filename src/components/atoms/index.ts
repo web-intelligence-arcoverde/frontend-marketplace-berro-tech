ad3224814e0c9f93b000ce88b6886ce660d6dd6f
Lucas Henrique <lukas.paes18@gmail.com>
2023-02-09 17:53:29 -0300

    refactor: exports

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,7 +2,7 @@ export {default as Logo} from './Logo';
 export {default as Button} from './Button';
 export {default as Search} from './Search';
 export {default as CardContact} from './CardContact';
-export {default as Avaliation} from './Avaliation';
-export {default as ButtonLink} from './Button/ButtonLink'
-export {default as ButtonAuthentication} from './Button/ButtonAuthentication'
-export {default as InputUser} from './Input/InputUser'
+export * from './Avaliation';
+export {default as ButtonLink} from './Button/ButtonLink';
+export {default as ButtonAuthentication} from './Button/ButtonAuthentication';
+export {default as InputUser} from './Input/InputUser';
