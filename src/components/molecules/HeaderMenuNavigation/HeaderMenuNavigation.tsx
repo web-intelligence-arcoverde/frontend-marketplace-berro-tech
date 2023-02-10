21e1700ec7efda15d82f0627a7d7e9c94322d35d
Lucas Henrique <lukas.paes18@gmail.com>
2023-02-10 11:22:57 -0300

    fix: header routers

--- src/components/molecules/HeaderMenuNavigation/HeaderMenuNavigation.tsx
+++ src/components/molecules/HeaderMenuNavigation/HeaderMenuNavigation.tsx
@@ -2,6 +2,8 @@ import {useRouter} from 'next/router';
 
 import {ContainerNavigation, ItemNavigation, Row} from './style';
 
+import Link from 'next/link';
+
 export const HeaderMenuNavigation = () => {
   const router = useRouter();
 
@@ -11,23 +13,19 @@ export const HeaderMenuNavigation = () => {
   let styleInit = optionInit ? 'brightness' : '';
   let styleBussiness = optionBussiness ? 'brightness' : '';
 
-  const handleClick = (routerPath: any) => {
-    router.push(routerPath);
-  };
-
   return (
     <ContainerNavigation>
       <ItemNavigation>
-        <li id={styleInit} onClick={() => handleClick('/')}>
-          Início
-        </li>
-        {optionInit && <Row />}
+        <Link href='/'>
+          <li id={styleInit}>Início</li>
+          {optionInit && <Row />}
+        </Link>
       </ItemNavigation>
       <ItemNavigation>
-        <li id={styleBussiness} onClick={() => handleClick('/negocios')}>
-          Negócios
-        </li>
-        {optionBussiness && <Row />}
+        <Link href='/negocios'>
+          <li id={styleBussiness}>Negócios</li>
+          {optionBussiness && <Row />}
+        </Link>
       </ItemNavigation>
     </ContainerNavigation>
   );
