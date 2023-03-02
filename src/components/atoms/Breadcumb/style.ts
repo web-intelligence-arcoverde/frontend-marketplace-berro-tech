90485447a8eca4be4527c1990c3583423408df27
Gab096 <Gabrielnogueira0000@hotmail.com>
2023-03-01 22:16:43 -0300

    cherry-pick(breadcumb): componentizando breadcumb

---
+++ src/components/atoms/Breadcumb/style.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { COLORS } from "@/common";
+
+export const ListBreadcumb = styled.ol`
+  display: flex;
+  padding: 16px;
+  overflow-x: auto;
+
+`
+export const ItemBreadcumb = styled.li`
+a{
+  font-weight: 400;
+  font-size: 14px;
+  color: ${COLORS.gray._01};
+}
+img{
+  margin: 0 14px;
+}
+:last-child{
+  a{
+    font-weight: 500;
+    color: ${COLORS.sub_brand._02};
+  }
+  img{
+    display: none;
+  }
+}
+`
--- src/pages/negocios/index.tsx
+++ src/pages/negocios/index.tsx
@@ -1,18 +1,17 @@
-import { Footer, Header } from "@/components";
-import { Main, Container } from "@/style";
+import { Breadcrumb, Footer, Header } from "@/components";
+import { BussinesContainer } from "@/style/negocios-style";
 
-const Business = () => {
+export const Business = () => {
+
   return (
-    <>
-      <Container>
-        <Main>
-          <Header />
-          <h2>Negocios</h2>
-        </Main>
-      </Container>
+    <div>
+      <Header />
+        <BussinesContainer>
+          <Breadcrumb />
+        </BussinesContainer>
       <Footer />
-    </>
+    </div>
   );
 };
 
-export default Business;
+export default Business
