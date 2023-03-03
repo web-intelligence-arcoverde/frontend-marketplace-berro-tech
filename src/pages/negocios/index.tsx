78fd64d7620c6dbe475aeba85e0db216c8ee605d
Gab096 <Gabrielnogueira0000@hotmail.com>
2023-03-02 21:56:58 -0300

    feat(tabs): estilizando tabs negocios

--- src/pages/negocios/index.tsx
+++ src/pages/negocios/index.tsx
@@ -1,14 +1,27 @@
-import { Breadcrumb, Footer, Header } from "@/components";
-import { BussinesContainer } from "@/style/negocios-style";
+import { Footer, Header, Tabs } from "@/components";
+import { BussinesContainer, ProductContainer, TabsContainer, SelectProduct, SelectContainer } from "@/style/negocios-style";
+import { BussinessHighlightProductMock, Bussinestabs } from "@/mock"
 
 export const Business = () => {
 
   return (
     <div>
       <Header />
-        <BussinesContainer>
-          <Breadcrumb />
-        </BussinesContainer>
+      <BussinesContainer>
+        <div>aside</div>
+        <ProductContainer>
+          <TabsContainer>
+            <Tabs tabs={Bussinestabs} />
+          </TabsContainer>
+          <SelectContainer>
+            <SelectProduct placeholder="Orderna por" name="ordanation" id="ordanation">
+              <option>Orderna por</option>
+              <option value="newer">Mais recentes</option>
+              <option value="oldest">Mais antigos</option>
+            </SelectProduct>
+          </SelectContainer>
+        </ProductContainer>
+      </BussinesContainer>
       <Footer />
     </div>
   );
