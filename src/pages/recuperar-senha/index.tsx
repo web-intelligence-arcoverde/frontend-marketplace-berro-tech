433a35f001bd221565fe18517fa29c66fcb9f5f9
João Paulo <joao.elesbao@g.globo.com>
2023-03-20 19:06:44 -0300

    feat(recovery-password): criado pagina para recuperar senha

---
+++ src/pages/recuperar-senha/index.tsx
@@ -0,0 +1,17 @@
+import { LayoutInit, MiniContainer } from "@/components";
+
+const RecoveryPassword = () => {
+  return (
+    <LayoutInit>
+      <MiniContainer
+        title="Esqueci minha senha"
+        subTitle="Siga os passos abaixo para recuperar"
+        lastButtonLink="dsddsdsdsdsd"
+      >
+        sas
+      </MiniContainer>
+    </LayoutInit>
+  );
+};
+
+export default RecoveryPassword;
