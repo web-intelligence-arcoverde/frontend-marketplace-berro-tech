8974791f0df0468b8a059a51d1d3a689382e9a79
João Paulo <joao.elesbao@g.globo.com>
2023-03-21 17:41:57 -0300

    fix(minhas-publicacoes: ajustado tamanho da foto e colocando tratamento para emails grandes nao quebrar o codigo)

--- src/components/molecules/CardPerfilVendedor/style.ts
+++ src/components/molecules/CardPerfilVendedor/style.ts
@@ -16,29 +16,30 @@ export const CardVendedor = styled.div`
   }
 `;
 
-export const HeaderCard = styled.header`
+export const HeaderCard = styled.div`
   display: flex;
   gap: 24px;
   border-bottom: solid 2px ${COLORS.light._02};
   padding: 25px 24px;
   align-items: center;
-  @media (max-width: ${BREAKPOINTS.sm}){
+  @media (max-width: ${BREAKPOINTS.sm}) {
     padding: 25px 0;
   }
 `;
 export const ImagePerfil = styled(Image)`
-  width: 90px;
-  height: 90px;
+  width: 96px;
+  height: 96px;
   object-fit: cover;
   border-radius: 5px;
 `;
 export const Aside = styled.aside`
   display: flex;
   flex-direction: column;
-  gap: 4px;
+  gap: 6px;
   h5 {
     color: ${COLORS.gray._02};
     font-size: 16px;
+    line-height: 24px;
     font-weight: 600;
   }
 
@@ -50,11 +51,16 @@ export const Aside = styled.aside`
   }
 `;
 
-export const TitleSection = styled.h6`
+export const TextGray = styled.h6`
   color: ${COLORS.gray._04};
   font-size: 14px;
   font-weight: 400;
   margin-bottom: 4px;
+  line-height: 21px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 200px;
 `;
 
 export const CardSection = styled.div`
@@ -64,13 +70,11 @@ export const CardSection = styled.div`
   padding: 24px;
   border-bottom: solid 1px ${COLORS.light._02};
 
-
   :last-child {
     border-bottom: none;
     padding-bottom: 0;
   }
   @media (max-width: ${BREAKPOINTS.sm}) {
-
     padding-left: 0;
 
     :first-child {
