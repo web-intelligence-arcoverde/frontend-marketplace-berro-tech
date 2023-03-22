1cd94357496a27cf66e5b9e72d2e53a4ac4fa596
João Paulo <joao.elesbao@g.globo.com>
2023-03-22 09:58:16 -0300

    feat(mocks): criado mocks de inputs

---
+++ src/mock/Inputs.ts
@@ -0,0 +1,46 @@
+export const SignInInputs = [
+  {
+    type: "email",
+    label: "Email",
+    name: "email",
+    placeholder: "Ex: rodrigo@exemplo.com",
+  },
+  {
+    type: "password",
+    label: "Senha",
+    name: "senha",
+    placeholder: "**********",
+  },
+];
+export const SignUpInputs = [
+  {
+    type: "text",
+    label: "Nome",
+    name: "name",
+    placeholder: "Seu nome completo",
+  },
+  {
+    type: "email",
+    label: "Email",
+    name: "email",
+    placeholder: "Seu melhor email",
+  },
+  {
+    type: "tel",
+    label: "Telefone",
+    name: "tel",
+    placeholder: "Ex: +55 12 99873-8767",
+  },
+  {
+    type: "password",
+    label: "Senha",
+    name: "password",
+    placeholder: "No mínimo 8 dígitos",
+  },
+  {
+    type: "password",
+    label: "Repita a senha",
+    name: "repeatPassword",
+    placeholder: "Repita a senha",
+  },
+];
