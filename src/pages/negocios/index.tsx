117fbbd844fe4ed96658c4f2117f5acec61c2c5f
João Paulo <joao.elesbao@g.globo.com>
2023-03-27 20:34:29 -0300

    feat(not-found): criado componente e condiçao pra quando nem tem negocio

--- src/pages/negocios/index.tsx
+++ src/pages/negocios/index.tsx
@@ -20,10 +20,21 @@ import { Bussinestabs } from "@/mock";
 import { Container, Main } from "@/style";
 import { useAppSelector } from "@/hooks/useSelectorHook";
 import { CardProductProps } from "@/types";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export const Business = () => {
   const allProduct = useAppSelector((state) => state.user.allProducts);
-  const currentSearch = useAppSelector((state)=> state.user.currentSearch)
+  const currentSearch = useAppSelector((state) => state.user.currentSearch);
+  const router = useRouter();
+  const routerBusiness = router.asPath;
+  const [empty, setEmpty] = useState(false);
+
+  useEffect(() => {
+    if (routerBusiness == "/negocios" && allProduct.length > 0) {
+      setEmpty(true);
+    }
+  }, []);
 
   return (
     <Container>
@@ -41,13 +52,25 @@ export const Business = () => {
               </SelectContainer>
             </ProductHeaderContainer>
             <ProductCardContainer>
-              {allProduct?.map((item: CardProductProps, index: number) => (
-                <ProductCard key={`${item.name} ${index}`} {...item} />
-              ))}
-              <NotFoundFilter
-                title={`Nenhum resultado para “${currentSearch}” `}
-                subtitle="Tente alterar os filtros para encontrar negócios"
-              />
+              {empty ? (
+                <>
+                  {allProduct.length > 0 ? (
+                    allProduct?.map((item: CardProductProps, index: number) => (
+                      <ProductCard key={`${item.name} ${index}`} {...item} />
+                    ))
+                  ) : (
+                    <NotFoundFilter
+                      title={`Nenhum resultado para “${currentSearch}” `}
+                      subtitle="Tente alterar os filtros para encontrar negócios"
+                    />
+                  )}
+                </>
+              ) : (
+                <NotFoundFilter
+                  title={`Não temos negócios no momento  `}
+                  subtitle={`Tente novamente mais tarde`}
+                />
+              )}
             </ProductCardContainer>
           </ProductContainer>
         </BussinesContainer>
