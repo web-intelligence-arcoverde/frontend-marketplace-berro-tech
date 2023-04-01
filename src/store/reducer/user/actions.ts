b08797d54ec4810e08dd2a2eefddf876283a7413
João Paulo <joao.elesbao@g.globo.com>
2023-04-01 19:37:26 -0300

    fix(redux): retirado action de product no user

--- src/store/reducer/user/actions.ts
+++ src/store/reducer/user/actions.ts
@@ -9,7 +9,6 @@ export const signInSuccess = createAction<{token: string}>(
   'user/sign-in-success',
 );
 
-export const addProduct = createAction<any>('ADD_PRODUCT');
 export const addUser = createAction<IRegisterUserProps>('ADD_USER');
 export const addPassword = createAction<IRegisterUserProps>('ADD_PASSWORD');
 export const loginSignProvider = createAction<any>('LOGIN_SIGN_PROVIDER');
