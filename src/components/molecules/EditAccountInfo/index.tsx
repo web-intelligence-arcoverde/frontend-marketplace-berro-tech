759f9d8c099a3259a153c8af258db85464db6b0b
Gab096 <Gabrielnogueira0000@hotmail.com>
2023-04-02 21:21:17 -0300

    feat

--- src/components/molecules/EditAccountInfo/index.tsx
+++ src/components/molecules/EditAccountInfo/index.tsx
@@ -1,36 +1,35 @@
-import { FloatingLabelInput, FloatingLabelRadio, FloatingLabelTextarea } from '@/components'
-import { TypeAnimal,Ovino,Caprino } from '@/mock'
-import React, { useState } from 'react'
+import { FloatingLabelInput } from '@/components'
+import React from 'react'
+import { Container } from './style'
 
 
 
 export const EditAccountInfo = () => {
-  const [animalOption, setAnimalOption] = useState('')
 
   return (
-    <div>
-        <FloatingLabelInput
-          required
-          type="text"
-          id="accountName"
-          name="accountName"
-          placeholder="Nome"
-        />
-        <FloatingLabelInput
-          required
-          type="text"
-          id="email"
-          name="email"
-          placeholder="E-mail"
-        />
-        <FloatingLabelInput
-          required
-          type="tel"
-          maxLength={11}
-          id="cellphone"
-          name="cellphone"
-          placeholder="Telefone"
-        />       
-          </div>
+    <Container>
+      <FloatingLabelInput
+        required
+        type="text"
+        id="accountName"
+        name="accountName"
+        placeholder="Nome"
+      />
+      <FloatingLabelInput
+        required
+        type="text"
+        id="email"
+        name="email"
+        placeholder="E-mail"
+      />
+      <FloatingLabelInput
+        required
+        type="tel"
+        maxLength={11}
+        id="cellphone"
+        name="cellphone"
+        placeholder="Telefone"
+      />
+    </Container>
   )
 }
