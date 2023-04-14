13c7d426d410bd0523f71e9bfc7219a9ec96f27b
Gab096 <Gabrielnogueira0000@hotmail.com>
2023-04-14 19:51:13 -0300

    fix(add-product):colocando formData do formulario de produto

--- src/components/molecules/RegisterInformation/index.tsx
+++ src/components/molecules/RegisterInformation/index.tsx
@@ -11,31 +11,24 @@ import {
   NextButtonContainer,
   NextButton,
 } from './style';
-import {IEditUserInfoProps} from '@/store/reducer/auth/types';
 import {CreateProductInfo} from '../CreateProductInfo';
 import {CreateProductBussines} from '../CreateProductBussines';
 
 const RegisterInformation = (props: any) => {
   const dispatch = useAppDispatch();
-  const formData = {} as ProductInfoType & IEditUserInfoProps;
-
+  const formData = new FormData()
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const form = event.target as HTMLFormElement;
     const inputs = form.querySelectorAll<HTMLInputElement>('[name]');
     inputs.forEach((input: any) => {
       const {name, value} = input;
-      if (name === 'price') {
-        formData[name] = removeMoneyMask(value);
-      } else if (input.type === 'file') {
-        formData[name] = input?.files[0];
-      } else {
-        formData[name] = value;
-      }
+        formData.append(name, value);
     });
-
+    
     dispatch(addProductInfo(formData));
     props.clickStep(2);
+  
   };
 
   return (
