05df55491d47add2a021815ea56b5b3e47afb086
Lucas Henrique <lukas.paes18@gmail.com>
2023-05-08 19:15:46 -0300

    refactor: mudando ip do service

--- src/pages/negocios/index.tsx
+++ src/pages/negocios/index.tsx
@@ -42,26 +42,6 @@ export const Business = () => {
     });
   });
 
-  function filterByAllAttributes(data, term) {
-    return data.filter((item) => {
-      for (const key in item) {
-        if (typeof item[key] === "object" && item[key] !== null) {
-          if (filterByAllAttributes([item[key]], term).length > 0) {
-            return true;
-          }
-        } else {
-          if (item[key] && item[key].toString().toLowerCase().includes(term.toLowerCase())) {
-            return true;
-          }
-        }
-      }
-      return false;
-    });
-  }
-
-
-  console.log(filterByAllAttributes(allProduct, 'Pedra'))
-
 
   useEffect(() => {
     if (routerBusiness == '/negocios' && allProduct && allProduct.length > 0) {
