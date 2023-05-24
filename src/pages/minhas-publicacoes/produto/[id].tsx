48f697cb4011c26dd7b8bb4ad97275c9ebb094b5
Lucas Henrique <lukas.paes18@gmail.com>
2023-05-23 23:49:14 -0300

    fix: ajustando formatacoes

--- src/pages/minhas-publicacoes/produto/[id].tsx
+++ src/pages/minhas-publicacoes/produto/[id].tsx
@@ -14,7 +14,7 @@ import {useAppSelector} from '@/hooks/useSelectorHook';
 import {readProductByIdRequest, removeProductModal} from '@/store/reducer/product/actions';
 import {Container, Main, StyleDesktop, StyleMobile} from '@/style';
 import {CardDescription, Content, InfoProduct} from '@/style/minhas-publicacoes-style';
-import {formatDate} from '@/util';
+import {formatDate, setWeightMask} from '@/util';
 import {useRouter} from 'next/router';
 import React, {useEffect} from 'react';
 import {useDispatch} from 'react-redux';
@@ -42,86 +42,92 @@ const Product = () => {
   }, [id, modal_update_product]);
 
 
-  return (<AuthPrivateRouter> {
-    loading_search_product_by_id ? (<></>) : (<Container>
-      <Header/>
-      <Main>
-        <Content>
-          <InfoProduct>
-            <PhotosAnimal arrayPhoto={
-              products.documents
-            }/>
-            <StyleDesktop>
-              <DescriptionProduct description={
-                  products.description
-                }
-                genetics={
-                  products.classification.name
-                }
-                weight={
-                  products.weight
-                }
-                birthDate={
-                  formatDate(new Date(products.date_birth))
+  return (
+    <AuthPrivateRouter> {
+      loading_search_product_by_id ? (
+        <></>
+      ) : (
+        <Container>
+          <Header/>
+          <Main>
+            <Content>
+              <InfoProduct>
+                <PhotosAnimal arrayPhoto={
+                  products.documents
                 }/>
-            </StyleDesktop>
-          </InfoProduct>
-          <CardDescription>
-            <ControlProduct id={
-                products.id
-              }
-              breed={
-                products.breed
-              }
-              business={
-                products.business
-              }
-              name={
-                products.name
-              }
-              address={
-                products.address
-              }
-              gender={
-                products.gender
-              }
-              ageCategory={
-                products.ageCategory
-              }
-              classification={
-                products.classification
-              }
-              maxWidth='100%'/>
-            <StyleMobile>
-              <DescriptionProduct description={
-                  products.description
-                }
-                genetics={
-                  products.classification.name
-                }
-                weight={
-                  products.weight
-                }
-                birthDate={
-                  formatDate(new Date(products.date_birth))
-                }/>
-            </StyleMobile>
-            <Notification/>
-          </CardDescription>
-          <StyleMobile>
-            <ButtonsControl editItem={
-                () =>
-                // @ts-ignore
-                setIsOpen(true)
-              }
-              removeItem={handleModalRemove}/>
-          </StyleMobile>
-          <ModalEditPublication/> {
-          controlModal && <ModalRemovePublication/>
-        } </Content>
-      </Main>
-    </Container>)
-  } </AuthPrivateRouter>);
+                <StyleDesktop>
+                  <DescriptionProduct description={
+                      products.description
+                    }
+                    genetics={
+                      products.classification.name
+                    }
+                    weight={
+                      products.weight
+                    }
+                    birthDate={
+                      formatDate(new Date(products.date_birth))
+                    }/>
+                </StyleDesktop>
+              </InfoProduct>
+              <CardDescription>
+                <ControlProduct id={
+                    products.id
+                  }
+                  breed={
+                    products.breed
+                  }
+                  business={
+                    products.business
+                  }
+                  name={
+                    products.name
+                  }
+                  address={
+                    products.address
+                  }
+                  gender={
+                    products.gender
+                  }
+                  ageCategory={
+                    products.ageCategory
+                  }
+                  classification={
+                    products.classification
+                  }
+                  maxWidth='100%'/>
+                <StyleMobile>
+                  <DescriptionProduct description={
+                      products.description
+                    }
+                    genetics={
+                      products.classification.name
+                    }
+                    weight={
+                      (products.weight.toString())
+                    }
+                    birthDate={
+                      formatDate(new Date(products.date_birth))
+                    }/>
+                </StyleMobile>
+                <Notification/>
+              </CardDescription>
+              <StyleMobile>
+                <ButtonsControl editItem={
+                    () =>
+                    // @ts-ignore
+                    setIsOpen(true)
+                  }
+                  removeItem={handleModalRemove}/>
+              </StyleMobile>
+              <ModalEditPublication/> {
+              controlModal && <ModalRemovePublication/>
+            } </Content>
+          </Main>
+        </Container>
+      )
+    } </AuthPrivateRouter>
+  );
 };
 
 export default Product;
